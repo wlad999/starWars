47b1ae92e5f417b03d2e16a153eb76a98fb33bd8
wlad999 <wladmail25@gmeil.com>
2020-06-15 21:52:31 +0300

    componentDidCatch(error, info)

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,22 +1,17 @@
 import React, { Component } from "react";
 import Header from "../header";
-import ItemList from "../item-list";
-import PersonDetails from "../person-details";
+import PeoplePage from "../people-page";
 import RandomPlanet from "../random-planet";
 import ErrorIndicator from "../error-indicator";
 
 export default class App extends Component {
   state = {
-    selectedPerson: 5,
     hasError: false,
   };
   componentDidCatch() {
     this.setState({ hasError: true });
   }
 
-  onItemSelected = (id) => {
-    this.setState({ selectedPerson: id });
-  };
   render() {
     if (this.state.hasError) {
       return <ErrorIndicator />;
@@ -25,14 +20,9 @@ export default class App extends Component {
       <div>
         <Header />
         <RandomPlanet />
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onItemSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
+        <PeoplePage />
+        <PeoplePage />
       </div>
     );
   }
