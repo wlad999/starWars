8de89dac26a39d6547783f0841bbe085856becd9
wlad999 <wlad25888@gmail.com>
2020-06-15 18:56:20 +0300

    added cDu

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../header";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import RandomPlanet from "../random-planet";
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+export default class App extends Component {
+  state = {
+    selectedPerson: 5,
+  };
+
+  onItemSelected = (id) => {
+    this.setState({ selectedPerson: id });
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onItemSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- src/component/random-planet/index.js
+++ src/component/random-planet/index.js
@@ -13,10 +13,12 @@ export default class RandomPlanet extends Component {
     loading: true,
     error: false,
   };
-  constructor() {
-    super();
+
+  componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 10000);
   }
+  
   onPlanetLoaded = (planet) => {
     this.setState({ planet, loading: false });
   };
@@ -24,20 +26,21 @@ export default class RandomPlanet extends Component {
     this.setState({ error: true, loading: false });
   };
 
-  updatePlanet() {
-    const id = Math.floor(Math.random() * 25) + 2;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random() * 25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError);
-  }
+    
+  };
   render() {
     const { planet, loading, error } = this.state;
     const hasData = !(loading || error);
     const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
     const content = hasData ? <PlanetView planet={planet} /> : null;
-
+    
     return (
       <div className="random-planet jumbotron rounded">
         {errorMessage}
