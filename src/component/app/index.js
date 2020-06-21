2100d1e020a5dc844b4969a1feb9e566ebe9385a
wlad999 <wladmail25@gmeil.com>
2020-06-21 22:36:01 +0300

    Render Function

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -32,6 +32,11 @@ export default class App extends Component {
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => (
+                <span>
+                  {item.name} <button>!</button>
+                </span>
+              )}
             />
           </div>
           <div className="col-md-6">
@@ -43,6 +48,7 @@ export default class App extends Component {
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}
             />
           </div>
           <div className="col-md-6">
