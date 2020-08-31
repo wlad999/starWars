228fa62f6fa1b0a1171de3b39d90bf9cf4cf3df3
wlad999 <wlad25888@gmail.com>
2020-08-31 16:28:52 +0300

    94 propTypes library added

--- src/component/item-list/index.js
+++ src/component/item-list/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types"
 import "./item-list.css";
 
 const ItemList = (props) => {
@@ -22,7 +23,13 @@ const ItemList = (props) => {
     return (<ul className="item-list list-group">{items}</ul>);
 }
 ItemList.defaultProps = {
-    onItemSelected:()=>{}
+    onItemSelected: () => {
+    }
+}
+ItemList.propTypes = {
+    onItemSelected: PropTypes.func,
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
+    children: PropTypes.func.isRequired
 }
 
 export default ItemList
